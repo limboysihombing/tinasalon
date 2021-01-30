fc2d0d1330707098a0c88871a770910423cd7f59
Limboy <limboysihombing@gmail.com>
2021-01-30 16:02:20 +0700

    update gallery

--- js/script.js
+++ js/script.js
@@ -19,3 +19,34 @@ $(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
+
+
+$(function () {
+  $(".galery-item").click(function () {
+    $('#modal').children().empty()
+    $('#modal').append(modalContent)
+    let imgUrl = $(this).css('background-image').replace(/^url\(['"](.+)['"]\)/, '$1');
+    let modalImg = `<img src="` + imgUrl + `" class="img-fluid" alt="Responsive image">`
+    $("#modal_img").append(modalImg)
+
+    let modalTitle = $(this).find('h4')[0].innerText
+    $('#modal').find("#modal_title").append(modalTitle)
+
+    $('#modal').modal('show')
+  })
+});
+let modalContent = `
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modal_title"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body p-0" id="modal_img">
+      </div>
+    </div>
+  </div>
+`
+
